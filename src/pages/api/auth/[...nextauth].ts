dd1c53bede85c2e65c0cba2f001bb0bc3e2f8211
Clecio de Almeida Junior <clecioaljunior@gmail.com>
2022-09-13 14:07:03 -0300

    feat: return userId in session

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -17,5 +17,13 @@ export default NextAuth({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
     })
-  ]
+  ],
+  callbacks: {
+    async session({ session, user }) {
+      return {
+        ...session,
+        userId: user.id
+      }
+    }
+  }
 })
