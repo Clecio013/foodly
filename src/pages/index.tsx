aacd993e331af03f590f35a110836ff0b012e0db
Clecio de Almeida Junior <clecioaljunior@gmail.com>
2022-09-12 21:10:34 -0300

    feat: push user to app when is logged

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,23 @@
 import Home from 'modules/Home'
+import { GetServerSideProps } from "next"
+import { getSession } from 'next-auth/react'
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const session = await getSession({ req })
+
+  if (session) {
+    return {
+      redirect: {
+        destination: '/dishes',
+        permanent: false
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
+}
 
 const HomePage = () => <Home />
 
