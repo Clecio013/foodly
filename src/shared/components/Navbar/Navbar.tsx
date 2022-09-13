8527405fe3a51dde167f617ead68c96a0d685e4b
Clecio de Almeida Junior <clecioaljunior@gmail.com>
2022-09-12 21:12:52 -0300

    feat: create navbar

---
+++ src/shared/components/Navbar/Navbar.tsx
@@ -0,0 +1,105 @@
+import { ReactNode } from 'react';
+import {
+  Box,
+  Flex,
+  Avatar,
+  HStack,
+  Link,
+  IconButton,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+  Text,
+} from '@chakra-ui/react';
+import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
+import { useSession, signOut } from 'next-auth/react';
+
+const Links = ['Pratos', 'Agendamentos'];
+
+const NavLink = ({ children }: { children: ReactNode }) => (
+  <Link
+    px={2}
+    py={1}
+    rounded={'md'}
+    _hover={{
+      textDecoration: 'none',
+      bg: useColorModeValue('gray.200', 'gray.700'),
+    }}
+    href={'#'}>
+    {children}
+  </Link>
+);
+
+const Navbar = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const session = useSession()
+
+  function handleSignOut() {
+    signOut({ callbackUrl: '/' })
+  }
+
+  return (
+    <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
+      <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+        <IconButton
+          size={'md'}
+          icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+          aria-label={'Open Menu'}
+          display={{ md: 'none' }}
+          onClick={isOpen ? onClose : onOpen}
+        />
+        <HStack spacing={8} alignItems={'center'}>
+          <HStack
+            as={'nav'}
+            spacing={4}
+            display={{ base: 'none', md: 'flex' }}>
+            {Links.map((link) => (
+              <NavLink key={link}>{link}</NavLink>
+            ))}
+          </HStack>
+        </HStack>
+        <Flex alignItems={'center'}>
+          <Menu>
+            <MenuButton>
+              <Text paddingRight={4}>{session?.data?.user?.name}</Text>
+            </MenuButton>
+            <MenuButton
+              as={Button}
+              rounded={'full'}
+              variant={'link'}
+              cursor={'pointer'}
+              minW={0}>
+              <Avatar
+                size={'sm'}
+                src={session?.data?.user?.image || session?.data?.user?.name || ''}
+              />
+            </MenuButton>
+            <MenuList>
+              <MenuItem>Configurações</MenuItem>
+              <MenuDivider />
+              <MenuItem onClick={handleSignOut}>Sair</MenuItem>
+            </MenuList>
+          </Menu>
+        </Flex>
+      </Flex>
+
+      {isOpen ? (
+        <Box pb={4} display={{ md: 'none' }}>
+          <Stack as={'nav'} spacing={4}>
+            {Links.map((link) => (
+              <NavLink key={link}>{link}</NavLink>
+            ))}
+          </Stack>
+        </Box>
+      ) : null}
+    </Box>
+  );
+}
+
+export default Navbar
