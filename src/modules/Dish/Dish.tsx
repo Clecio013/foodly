817f69d8264c6cc623703da6aef8ef6d0aea1906
Clecio de Almeida Junior <clecioaljunior@gmail.com>
2022-09-13 21:28:35 -0300

    feat: create dish detail

---
+++ src/modules/Dish/Dish.tsx
@@ -0,0 +1,71 @@
+import { Dish as DishProps } from "@prisma/client"
+import Layout from "shared/components/Layout"
+import { Heading, Text, Image, Container, Button, FormControl, FormLabel, Input, useToast } from "@chakra-ui/react"
+import { useSession } from "next-auth/react"
+import api from "services/axios"
+import { ChangeEvent, useState } from "react"
+import { useRouter } from "next/router"
+
+const Dish = ({ title, description, imageUrl, id }: DishProps) => {
+  const { data: session } = useSession()
+  const [appointmentDate, setAppointmentDate] = useState('')
+  const toast = useToast()
+  const router = useRouter()
+
+  function handleChangeDate(event: ChangeEvent<HTMLInputElement>) {
+    const date = event.target.value
+    const isoDate = new Date(date).toISOString()
+
+    setAppointmentDate(isoDate)
+  }
+
+  function handleCreateAppointment() {
+    api.post('/appointment/create', {
+      userId: session?.user.userId,
+      dishId: id,
+      date: appointmentDate
+    })
+      .then(() => {
+        toast({
+          title: 'Oba, o agendamento foi feito com sucesso',
+          description: "Entraremos em contato em breve para bater um papo sobre o agendamento",
+          status: 'success',
+          duration: 9000,
+          isClosable: true,
+          position: 'top-right'
+        })
+        router.push(`success/?date=${appointmentDate}`)
+      })
+      .catch(() => {
+        toast({
+          title: 'Houve um erro fazer o agendamento',
+          description: "Mas não se preocupe, já fomos avisados e entraremos em contato em breve",
+          status: 'error',
+          duration: 9000,
+          isClosable: true,
+          position: 'top-right'
+        })
+      })
+  }
+
+  return (
+    <>
+      <Layout>
+        <Container maxW={'container.lg'}>
+          <Image src={imageUrl}  width='100%' maxHeight='400px' bgSize={'cover'}/>
+          <Heading>{title}</Heading>
+          <Text paddingBottom={'4'}>{description}</Text>
+
+          <FormControl paddingBottom={'4'}>
+            <FormLabel>Escolha a data que deseja agendar</FormLabel>
+            <Input type='date' onChange={handleChangeDate} />
+          </FormControl>
+
+          <Button width={'full'} onClick={handleCreateAppointment}>Adicionar</Button>
+        </Container>
+      </Layout>
+    </>
+  )
+}
+
+export default Dish
--- src/modules/Dishes/Card/Card.tsx
+++ src/modules/Dishes/Card/Card.tsx
@@ -1,15 +1,16 @@
 import { Text, Box, Image } from '@chakra-ui/react'
 import type { Dish } from '@prisma/client'
+import Link from 'next/link'
 
-const Card = ({ title, description, imageUrl }: Dish) => {
-  return (
+const Card = ({ id, title, description, imageUrl }: Dish) => (
+  <Link href={`/dishes/${id}`}>
     <Box>
       <Image src={imageUrl} borderRadius='2xl' marginBottom={'2'} />
 
       <Text fontSize="lg" fontWeight='bold' paddingBottom={'1'}>{title}</Text>
       <Text>{description}</Text>
     </Box>
-  )
-}
+  </Link>
+)
 
 export default Card
