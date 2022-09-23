f84dc9badad46efb7fcc14840e17761571ab7694
Clecio de Almeida Junior <clecioaljunior@gmail.com>
2022-09-22 22:23:11 -0300

    feat: add type to session

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,19 @@ import { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { SessionProvider } from 'next-auth/react'
 import { theme } from 'theme'
+import { DefaultSession } from 'next-auth'
 
-const App = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => {
+type Props = AppProps & {
+  pageProps: {
+    session: DefaultSession & {
+      user: {
+        userId: string
+      }
+    }
+  }
+}
+
+const App = ({ Component, pageProps: { session, ...pageProps } }: Props) => {
   return (
     <SessionProvider session={session}>
       <ChakraProvider theme={theme}>
